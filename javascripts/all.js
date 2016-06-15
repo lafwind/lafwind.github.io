b9aa7bd09641fb217683ca85d8f0f6a3c6ad21ad
Lafwind <lafwind@gmail.com>
2016-06-15 14:59:14 +0800

    完成页面重设计(包括响应式设计、结构微调)

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".arrow").click(function(){$("html, body").animate({scrollTop:$(".content").offset().top},600)})});
+$(document).ready(function(){$(".arrow").click(function(){$("html, body").animate({scrollTop:$(".content").offset().top},600)}),$(".handle").on("click",function(){$("nav").toggleClass("show"),$(".fa").toggleClass("fa-bars"),$(".fa").toggleClass("fa-minus")})});
